584783c59e5c10773e0a56d17b1c09b79feec647
Muhammad <76010638+MuhammedAtta@users.noreply.github.com>
2022-05-17 00:48:44 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# PortfolioWebsite
-This is a portfolio website i have created, you can easily use it for your own portfolio the cods are pretty easy to read, you can edit them how you want.
+# mat1nm.github.io
---
+++ js/index.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+      document.body.classList.toggle('nav-open');
+
+});
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+
+    })
+})
